ed38345a87d0194e4dba82504ef970320144b976
unknown <stevenbennett90@gmail.com>
2023-02-01 14:23:35 -0800

    works not, not well but works

--- grades/app.py
+++ grades/app.py
@@ -9,10 +9,13 @@ import logging
 
 app = Flask(__name__, template_folder='./templates', static_folder='./static')
 
+app.secret_key = 'secret key'
+
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'pass'
+app.config['MYSQL_PASSWORD'] = 'Password'
 app.config['MYSQL_DB'] = 'grades'
+
 mysql = MySQL(app)
 
 """ 
@@ -23,27 +26,29 @@ mysql.connection.commit()
 cursor.close()
 """
 
-@app.route('/gpa', methods=['POST','GET'])
-def hello():
-    user_id = request.cookies.get('user_id')
-    if not user_id:
-        user_id = None
-    
-    vote = None
-
-    if request.method == 'POST':
+@app.route('/')
+@app.route('/submit', methods=['GET','POST'])
+def submit():
+#    user_id = request.cookies.get('user_id')
+#    if not user_id:
+#        user_id = None
+    msg = ''
+    if request.method == 'POST' and 'user' in request.form and 'course1' in request.form:
+        user = request.form['user']
         course1 = request.form['course1']
         course2 = request.form['course2']
         course3 = request.form['course3']
         course4 = request.form['course4']
         course5 = request.form['course5']
         cursor = mysql.connection.cursor()
-        cursor.execute(''' INSERT INTO info_table VALUES(%s,%s)''',(course1,course2,course3,course4,course5))
+        cursor.execute(''' INSERT INTO grades VALUES(NULL,%s,%s,%s,%s,%s,%s)''',(user,course1,course2,course3,course4,course5))
         mysql.connection.commit()
         cursor.close()
-        return render_template('gpa_calc.html'), 201
+        msg = 'Success'
+        return render_template('index.html', msg=msg), 201
 
-app.run(host='localhost', app=5000)
+    return render_template('index.html'), 201
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='localhost', app=5000, debug=True)
