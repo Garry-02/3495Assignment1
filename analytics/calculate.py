0cc25ab80add3bc37e4deff2e8ec1c9ca851a3e0
Garry-02 <gurjyotsingh76@gmail.com>
2023-02-02 13:17:16 -0800

    added analytics section(unfinished)

---
+++ analytics/calculate.py
@@ -0,0 +1,23 @@
+# reading the data from the sql database
+
+# perform simple calculations on the data
+#def calculator(data): 
+#    max_value = 0
+#    min_value = 100
+#    sum_of_percentages = 0
+#    number_of_percantages = 0 
+
+#    for data in data_from_database:
+#        if data[value] > max_value:
+#            max_value=data[value]
+#        if data[value] < min_value:
+#            min_value=data[min_value]
+
+#        sum_of_percentages += data[value]
+#        number_of_percantages += 1
+
+#    average_value = sum_of_percentages/number_of_percantages
+
+#    return max_value, min_value, average_value
+
+# sending the data to the mongo db 
