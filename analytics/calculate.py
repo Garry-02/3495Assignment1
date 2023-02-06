639481243e29b4cfc743a97b1ccdb3ff41da54e4
Garry-02 <gurjyotsingh76@gmail.com>
2023-02-06 12:27:32 -0800

    calculations

--- analytics/calculate.py
+++ analytics/calculate.py
@@ -1,47 +1,54 @@
-# reading the data from the sql database
-
-# perform simple calculations on the data
-#def calculator(data): 
-#    max_value = 0
-#    min_value = 100
-#    sum_of_percentages = 0
-#    number_of_percantages = 0 
-
-#    for data in data_from_database:
-#        if data[value] > max_value:
-#            max_value=data[value]
-#        if data[value] < min_value:
-#            min_value=data[min_value]
-
-#        sum_of_percentages += data[value]
-#        number_of_percantages += 1
-
-#    average_value = sum_of_percentages/number_of_percantages
-
-#    return max_value, min_value, average_value
-
-# sending the data to the mongo db 
-
-
-from typing import List, Dict
-from flask import Flask, request, render_template
 import mysql.connector
-import json
+import pymongo
+import os
+from pymongo import MongoClient
 
-app = Flask(__name__)
-app.secret_key = 'secret key'
+config = {
+    'user': 'root',
+    'password': 'root',
+    'host': 'db',
+    'port': '3306',
+    'database': 'grades'
+}
 
-@app.route('/')
-def home():
-    return "Hello world!"
+connection = mysql.connector.connect(**config)
 
-@app.route('/submit', methods=['GET','POST'])
-def submit():
-    msg = ''
-    config = {
-        'user': 'root',
-        'password': 'root',
-        'host': 'db',
-        'port': '3306',
-        'database': 'grades'
-    }
+cursor = connection.cursor()
+query = "SELECT course1, course2, course3, course4, course5 from GRADES where user_id is {user_id};"
+cursor.execute(query)
+grades = []
+for grade in cursor:
+    grades.append(grade)
+
+max_value = 0
+min_value = 100
+sum_of_grades = 0
+number_of_grades = 0
+for grade in grades:
+
+    for data in grades:
+        if data > max_value:
+            max_value=data
+        if data < min_value:
+            min_value=data
+
+        sum_of_grades += data
+        number_of_grades += 1
+
+    average_value = sum_of_grades/number_of_grades
+
+    
+client = MongoClient(os.environ["MONGO_DB_URI"])
+db = client[os.environ["MONGO_DB_NAME"]]
+
+# Write the analytics to MongoDB
+db.analytics.insert_one({
+    "min_grade": min_value,
+    "max_grade": max_value,
+    "avg_grade": average_value
+})
+
+# Close the connections
+cursor.close()
+connection.close()
+# sending the data to the mongo db 
