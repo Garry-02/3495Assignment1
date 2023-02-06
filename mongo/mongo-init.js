d08aee61f2237b1af121b22a8cbeea6854f9d5b5
sukhmanp9090 <123767157+sukhmanp9090@users.noreply.github.com>
2023-02-06 13:05:52 -0800

    Update mongo-init.js

--- mongo/mongo-init.js
+++ mongo/mongo-init.js
@@ -1,21 +1,2 @@
 
-db = db.getSiblingDB("animal_db");
-db.animal_tb.drop();
-
-db.animal_tb.insertMany([
-    {
-        "id": 1,
-        "name": "Lion",
-        "type": "wild"
-    },
-    {
-        "id": 2,
-        "name": "Cow",
-        "type": "domestic"
-    },
-    {
-        "id": 3,
-        "name": "Tiger",
-        "type": "wild"
-    },
-]);
+db = db.getSiblingDB("grades_db");
