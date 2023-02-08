cd5128365ba0102c8b55367887cc350049e8904a
unknown <stevenbennett90@gmail.com>
2023-02-07 22:31:09 -0800

    fixed somewhat

--- result/server.js
+++ result/server.js
@@ -1,53 +1,40 @@
+const { json } = require('express');
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const mongoose = require('mongoose');
 
 const app = express();
-
 const PORT = 5002;
 const HOST = '0.0.0.0';
 
+// Set the view engine
+app.set('view engine', 'ejs');
 
 // Connect to the MongoDB database
 
 const uri = "mongodb://root:root@test_mongodb:27017/grades_db?authSource=admin";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const studentResultSchema = new mongoose.Schema({
-  minGrade: Number,
-  maxGrade: Number,
-  averageGrade: Number
-});
 
-const StudentResult = mongoose.model('StudentResult', studentResultSchema);
+app.get('/results', async (req, res) => {
+  const db = client.db('grades_db');
+  const analytics = db.collection('analytics');
 
-app.get("/analytics", async (req, res) => {
-  const client = new MongoClient(uri, { useNewUrlParser: true });
+  // Retrieve the analytics data from the database
+  const data = await analytics.find({}).toArray();
 
-  try {
-    await client.connect();
-
-    const db = client.db("grades_db");
-    const analytics = db.collection("analytics");
-    const analyticsData = await analytics.find().toArray();
-
-    res.send(analyticsData);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send(error);
-  } finally {
-    client.close();
-  }
-});
-
-// Route to get the results
-/* app.get('/results', (req, res) => {
-  StudentResult.find({}, (err, results) => {
-    if (err) {
-      return res.status(500).send(err);
-    }
-    return res.send(results);
+  // Format the data for the HTML table
+  const results = data.map(result => {
+    return {
+      minGrade: result.min_grade,
+      maxGrade: result.max_grade,
+      averageGrade: result.avg_grade
+    };
   });
-}); */
+
+  // Send the results as JSON
+  res.render('./template.ejs', { results });
+});
 
 // Start the Express server
 app.listen(PORT, HOST, () => {
